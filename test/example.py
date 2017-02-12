2a445bfd27137d4b988d40507996f2d911756b35
Rodrigo Prado de Jesus <royopa@gmail.com>
2017-02-11 23:14:38 -0200

    changed example test

--- test/example.py
+++ test/example.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import sys
+sys.path.insert(0, './../python-cobol')
+
 import cobol
 
 with open("example.cbl",'r') as f:
     for row in cobol.process_cobol(f.readlines()):
-    	print row['name']
+        print(row['name'])
